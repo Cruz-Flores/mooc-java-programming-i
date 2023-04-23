9b7652e6aa2adf5a63ea29f9cb383d919bedfc37
Cruz Flores <cruz.tag@gmail.com>
2023-04-23 17:48:17 -0600

    AVClub resolved

--- part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -5,14 +5,21 @@ public class AVClub {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String sentence = scanner.nextLine();
-        String[] pieces = sentence.split(" ");
-        int i = 0;
-        while (i < pieces.length) {
-            if (pieces[i].contains("av")) {
-                System.out.println(pieces[i]);
+
+        while (true) {
+            String sentence = scanner.nextLine();
+
+            if (sentence.equals("")) {
+                break;
             }
-            i++;
+
+            String[] pieces = sentence.split(" ");
+            for (String word : pieces) {
+                if (word.contains("av")) {
+                    System.out.println(word);
+                }
+
+            }
         }
     }
 }
