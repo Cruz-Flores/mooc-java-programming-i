84249e815d9ec0f773b47a0bc31b62edc957e946
Cruz Flores <cruz.tag@gmail.com>
2023-04-23 20:59:41 -0600

    counting to hundred resolved

--- part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -5,25 +5,36 @@ public class CountingToHundred {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int number = Integer.valueOf(scanner.next());
 
-        int count = number;
-        boolean isShowedTheMessage = false;
-        while (count <= 100) {
-
-            if (count < -1 || count >= 98) {
-                System.out.println(count);
-            }
-
-            if (count >= -1 && count < 98) {
-                if (!isShowedTheMessage) {
-                    System.out.println("...many numbers...");
-                    isShowedTheMessage = true;
-                }
-            }
-
-            count = count + 1;
-
+        int numberToPrint = Integer.valueOf(scanner.nextLine());
+        for (int i = numberToPrint; i <= 100; i++) {
+            System.out.println(i);
         }
+
     }
 }
+
+// import java.util.Scanner;
+
+// public class CountingToHundred {
+
+// public static void main(String[] args) {
+// Scanner scanner = new Scanner(System.in);
+// if (scanner.hasNextInt()) {
+// int number = scanner.nextInt();
+// if (number < 100) {
+// for (int i = number; i <= 100; i++) {
+// System.out.println(i);
+// }
+// }
+// } else {
+// System.out.println("Error: Input is not an integer.");
+// }
+
+// }
+// }
+
+// while (numberToPrint <= 100) {
+// System.out.println(numberToPrint);
+// numberToPrint++;
+// }
