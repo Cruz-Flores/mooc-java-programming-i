621cd70eead9989f52baf6a0c69d32e51dcd9d80
Cruz Flores <cruz.tag@gmail.com>
2023-04-23 21:31:50 -0600

    age of the oldest resolved

--- part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -6,6 +6,24 @@ public class AgeOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int maxAge = 0;
+        while (true) {
+            String personalData = scanner.nextLine();
+            if (personalData.equals("")) {
+                break;
+            }
+            String[] parts = personalData.split(",");
+
+            int currentAge = Integer.valueOf(parts[1]);
+
+            if (currentAge > maxAge) {
+                System.out.println(currentAge);
+                maxAge = currentAge;
+            }
+
+        }
+
+        System.out.println("Age of the oldest: " + maxAge);
 
     }
 }
