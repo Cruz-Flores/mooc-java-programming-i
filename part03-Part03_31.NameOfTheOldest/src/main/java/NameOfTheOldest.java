8ad9fb03a3565c58330bf7dd7318538645fb6fc8
Cruz Flores <cruz.tag@gmail.com>
2023-04-23 22:03:36 -0600

    the oldest resolved

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -6,6 +6,24 @@ public class NameOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String theOldest = "";
+        int maxAge = 0;
+        while (true) {
+            String personalData = scanner.nextLine();
+            if (personalData.equals("")) {
+                break;
+            }
+
+            String[] words = personalData.split(",");
+            int age = Integer.valueOf(words[1]);
+            if (age > maxAge) {
+                theOldest = words[0];
+                maxAge = age;
+            }
+
+        }
+
+        System.out.println("Name of the oldest: " + theOldest);
 
     }
 }
