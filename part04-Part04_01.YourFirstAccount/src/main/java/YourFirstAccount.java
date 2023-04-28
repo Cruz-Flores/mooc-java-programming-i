32348b001c5de15a28e2c31e2212e45b8e0d482e
Cruz Flores <cruz.tag@gmail.com>
2023-04-27 23:40:19 -0600

    your first account resolved

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,9 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        Account cruzAccount = new Account("Cruz Account", 100.0);
+        cruzAccount.deposit(20.0);
+        System.out.println(cruzAccount);
+
     }
 }
