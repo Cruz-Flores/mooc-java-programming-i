4fb383ddf166bdaacc137b4eae0ba34392480973
Cruz Flores <cruz.tag@gmail.com>
2023-05-28 15:18:40 -0600

    film resolved

---
+++ part04-Part04_11.Film/src/main/java/Film.java
@@ -0,0 +1,17 @@
+public class Film {
+  private String name;
+  private int filmAgeRating;
+
+  public Film(String name, int filmAgeRating) {
+    this.name = name;
+    this.filmAgeRating = filmAgeRating;
+  }
+
+  public String name() {
+    return this.name;
+  }
+
+  public int ageRating() {
+    return this.filmAgeRating;
+  }
+}
