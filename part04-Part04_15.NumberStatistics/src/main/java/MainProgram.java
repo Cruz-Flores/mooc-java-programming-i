2f38a492cc1a358f096b6cb9003259ec4a0383f2
Cruz Flores <cruz.tag@gmail.com>
2023-06-02 21:33:52 -0600

    Statistics resolved

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -5,12 +5,43 @@ public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // you can write test code here
-        // however, remove all unnecessary code when doing the final parts of the exercise
+        Statistics statistics = new Statistics();
 
-        // In order for the tests to work, the objects must be created in the
-        // correct order in the main program. First the object that tracks the total
-        // sum, secondly the object that tracks the sum of even numbers, 
-        // and lastly the one that tracks the sum of odd numbers!
+        Statistics evenStatistics = new Statistics();
+
+        Statistics oddStatistics = new Statistics();
+
+        System.out.println("Enter numbers:");
+
+        while (true) {
+            int number = scanner.nextInt();
+            if (number == -1) {
+                break;
+            }
+
+            if (number % 2 == 0) {
+                evenStatistics.addNumber(number);
+            }
+
+            if (number % 2 != 0) {
+                oddStatistics.addNumber(number);
+            }
+
+            statistics.addNumber(number);
+        }
+
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Sum of even numbers: " + evenStatistics.sum());
+        System.out.println("Sum of odd numbers: " + oddStatistics.sum());
     }
 }
+
+// model solution
+
+// sum.addNumber(number);
+
+// if (number % 2 == 0) {
+// evenSum.addNumber(number);
+// } else {
+// oddSum.addNumber(number);
+// }
